42991189e09c267eda453cd2b3d816580e4a9dfa
Mirko <mirkoj1996@gmail.com>
2020-01-27 02:05:06 +0100

    xmonad, xmobar and stalonetray

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,38 @@
+import XMonad
+import XMonad.Config.Desktop
+import System.IO
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+
+
+main = do
+  xmproc <- spawnPipe "xmobar"
+  xmonad $ desktopConfig
+    { terminal             = myTerminal
+    , manageHook           = manageDocks <+> myManageHook <+> manageHook desktopConfig
+    , borderWidth          = myBorderWidth
+    , layoutHook           = avoidStruts $ layoutHook desktopConfig
+    , logHook              = dynamicLogWithPP xmobarPP
+      { ppOutput = hPutStrLn xmproc
+      , ppTitle  = xmobarColor "#bababa" "" . shorten 50
+      }
+    } `additionalKeys` myKeys
+
+myTerminal    = "termite"
+
+myBorderWidth = 1
+
+myManageHook  = composeAll
+  [ className =? "Gimp"        --> doFloat
+  , className =? "Arandr"      --> doFloat
+  , className =? "keepassx2"   --> doFloat
+  , className =? "qutebrowser" --> doShift "2"
+  ]
+
+myKeys =
+  [ ((mod1Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+  , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+  , ((0, xK_Print), spawn "scrot")
+  ]
