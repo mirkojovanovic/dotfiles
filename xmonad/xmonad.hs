8ad25c4a15e2cc02984be8971f51d71833bd7dba
Mirko <mirkoj1996@gmail.com>
2020-02-01 04:56:39 +0100

    xmonad P keys, xmobar dynamic dimensions and single cpu percentage, stalonetray geometry +1 (still static dimensions)

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,40 +1,76 @@
+
+import System.IO
+import Data.Maybe (isJust)
+import qualified Data.Map.Strict as Map
+import qualified XMonad.StackSet as W
+
+import XMonad.Actions.CycleWS (moveTo, shiftTo, WSType(..), shiftNextScreen, shiftPrevScreen)
+
 import XMonad
 import XMonad.Config.Desktop
-import System.IO
+
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
 
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeysP)
+
+import XMonad.Layout.PerWorkspace
+import XMonad.Layout.NoBorders
 
 main = do
   xmproc <- spawnPipe "xmobar"
   xmonad $ desktopConfig
-    { terminal             = myTerminal
-    , manageHook           = manageDocks <+> myManageHook <+> manageHook desktopConfig
-    , borderWidth          = myBorderWidth
-    , layoutHook           = avoidStruts $ layoutHook desktopConfig
-    , logHook              = dynamicLogWithPP xmobarPP
+    { terminal    = myTerminal
+    , workspaces  = myWorkspaces
+    , modMask     = myModMask
+    , manageHook  = manageDocks <+> myManageHook <+> manageHook desktopConfig
+    , layoutHook  = myLayoutHook
+    , borderWidth = myBorderWidth
+    , normalBorderColor = myNormalBorderColor
+    , focusedBorderColor = myFocusedBorderColor
+    , logHook     = dynamicLogWithPP xmobarPP
       { ppOutput = hPutStrLn xmproc
       , ppTitle  = xmobarColor "#bababa" "" . shorten 50
+      , ppCurrent = xmobarColor "#d75f5f" "" . wrap "[" "]"
       }
-    } `additionalKeys` myKeys
+    } `additionalKeysP` myKeys
 
 myTerminal    = "termite"
+myModMask     = mod4Mask
 
 myBorderWidth = 1
+myNormalBorderColor = "#1c1c1c"
+myFocusedBorderColor = "#f799d7"
+
+myWorkspaces  = ["term", "web", "dev", "chat", "media", "read"] ++ map show [7..9]
 
 myManageHook  = composeAll
   [ className =? "Gimp"        --> doFloat
   , className =? "Arandr"      --> doFloat
   , className =? "keepassx2"   --> doFloat
-  , className =? "qutebrowser" --> doShift "2"
-  , className =? "discord"     --> doShift "3"
-  , className =? "slack"       --> doShift "3"
+  , className =? "qutebrowser" --> doShift "web"
+  , className =? "discord"     --> doShift "chat"
+  , className =? "slack"       --> doShift "chat"
   ]
 
 myKeys =
-  [ ((mod1Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
-  , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-  , ((0, xK_Print), spawn "scrot")
-  ]
+  [ ("M-S-z"     , spawn "~/.local/bin/lock_screen")
+
+  -- Taking screenshots
+  , ("C-<Print>" , spawn "sleep 0.2; scrot -s")
+  , ("<Print>"   , spawn "scrot")
+
+  -- Workspaces
+  , ("M-l", moveTo Next nonNSP)
+  , ("M-h", moveTo Prev nonNSP)
+  , ("M-S-l", shiftTo Next nonNSP >> moveTo Next nonNSP)
+  , ("M-S-h", shiftTo Prev nonNSP >> moveTo Prev nonNSP)
+  ] where nonNSP = WSIs (return (\ws -> W.tag ws /= "nsp"))
+          nonEmptyNonNSP  = WSIs (return (\ws -> isJust (W.stack ws) && W.tag ws /= "nsp"))
+
+
+myLayoutHook = avoidStruts
+             $ smartBorders
+             $ onWorkspace "chat" Full
+             $ layoutHook desktopConfig
